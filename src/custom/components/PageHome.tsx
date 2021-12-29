ff6cf664b168ba878801c07b5cd2002da99e18bc
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-29 17:22:15 +0100

    add siteNode

--- src/custom/components/PageHome.tsx
+++ src/custom/components/PageHome.tsx
@@ -1,10 +1,12 @@
 import '../styles/pageHome.scss';
+import * as config from '../../system/config';
 
 function PageHome() {
 
 	return (
 		<div className="page page_home">
 			<p>Welcome to this site.</p>	
+			<p>{config.getSiteMode()}</p>
 		</div>
 	)
 }
--- src/system/config.ts
+++ src/system/config.ts
@@ -1,3 +1,8 @@
 export const getBackendPort = () => {
 	return 6731;
 }
+
+export const getSiteMode = () => {
+	const siteMode = process.env.REACT_APP_SITEMODE ? process.env.REACT_APP_SITEMODE : 'production';
+	return siteMode;
+} 
