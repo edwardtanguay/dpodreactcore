b4de7b7da7bdccf7bc94c1bb4259bdb3f9d6475e
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-30 14:19:15 +0100

    interface

---
+++ src/custom/models/interfaces.ts
@@ -0,0 +1,9 @@
+export interface IHowto {
+	id: number;
+	categoryTitle: string;
+	title: string;
+	systemWhenCreated: string;
+	systemWhoCreated: string;
+	bodyParsed: string;
+	jsfiddleUrl: string;
+}
