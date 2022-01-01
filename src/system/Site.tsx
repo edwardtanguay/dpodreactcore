aa5ca0d2e574f28fc4ab0ddb5539999798e8b8bc
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-01 18:50:05 +0100

    fixing: three work

--- src/system/Site.tsx
+++ src/system/Site.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import './styles/reset.scss';
 import './styles/site.scss';
 import './styles/dpod.scss';
@@ -10,25 +10,25 @@ import * as qarr from '../system/qtools/qarr';
 import PageShowcaseCounterUsingState from './pages/PageShowcaseCounterUsingState'; // ::showcaseCounterUsingState
 import PageShowcaseCounterUsingReducer from './pages/PageShowcaseCounterUsingReducer';// ::showcaseCounterUsingReducer
 import PageShowcaseTypeScriptClasses from './pages/PageShowcaseTypeScriptClasses';// ::showcaseTypeScriptClasses
-import PageShowcaseJavaScriptComponent from './pages/PageShowcaseJavaScriptComponent';// ::showcaseJavaScriptComponent
-import PageShowcaseSqliteReader from './pages/PageShowcaseSqliteReader';// ::showcaseSqliteReader
-import PageShowcaseJsonReadWrite from './pages/PageShowcaseJsonReadWrite';// ::showcaseJsonReadWrite
-import PageDeletePage from './pages/PageDeletePage';// ::deletePage
-import PageCreatePage from './pages/PageCreatePage';// ::createPage
-import PageShowcaseNewsApi from './pages/PageShowcaseNewsApi';// ::showcaseNewsApi
-import PageManageShowcaseReports from './pages/PageManageShowcaseReports';// ::manageShowcaseReports
-import PageShowcaseUseToggle from './pages/PageShowcaseUseToggle';// ::showcaseUseToggle
-import PageShowcaseClassInState from './pages/PageShowcaseClassInState';// ::showcaseClassInState
-import PageShowcaseFetchTryCatch from './pages/PageShowcaseFetchTryCatch';// ::showcaseFetchTryCatch
-import PageShowcaseReactIcons from './pages/PageShowcaseReactIcons';// ::showcaseReactIcons
-import PageCurriculumFlashcardParser from './pages/PageCurriculumFlashcardParser';// ::curriculumFlashcardParser
-import PageImportAssessmentJSONIntoSQLite from './pages/PageImportAssessmentJSONIntoSQLite';// ::importAssessmentJSONIntoSQLite
-import PageShowcaseSampleDataWithFaker from './pages/PageShowcaseSampleDataWithFaker';// ::showcaseSampleDataWithFaker
-import PageShowcaseMongoDBDriverCRUD from './pages/PageShowcaseMongoDBDriverCRUD';// ::showcaseMongoDBDriverCRUD
-import PageGenerateMockData from './pages/PageGenerateMockData';// ::generateMockData
-import PageShowcaseMongooseCRUD from './pages/PageShowcaseMongooseCRUD';// ::showcaseMongooseCRUD
-import PageHowtos from '../custom/pages/PageHowtos';// ::howtos
-import PageFlashcards from './pages/PageFlashcards';// ::flashcards
+// import PageShowcaseJavaScriptComponent from './pages/PageShowcaseJavaScriptComponent';// ::showcaseJavaScriptComponent
+// import PageShowcaseSqliteReader from './pages/PageShowcaseSqliteReader';// ::showcaseSqliteReader
+// import PageShowcaseJsonReadWrite from './pages/PageShowcaseJsonReadWrite';// ::showcaseJsonReadWrite
+// import PageDeletePage from './pages/PageDeletePage';// ::deletePage
+// import PageCreatePage from './pages/PageCreatePage';// ::createPage
+// import PageShowcaseNewsApi from './pages/PageShowcaseNewsApi';// ::showcaseNewsApi
+// import PageManageShowcaseReports from './pages/PageManageShowcaseReports';// ::manageShowcaseReports
+// import PageShowcaseUseToggle from './pages/PageShowcaseUseToggle';// ::showcaseUseToggle
+// import PageShowcaseClassInState from './pages/PageShowcaseClassInState';// ::showcaseClassInState
+// import PageShowcaseFetchTryCatch from './pages/PageShowcaseFetchTryCatch';// ::showcaseFetchTryCatch
+// import PageShowcaseReactIcons from './pages/PageShowcaseReactIcons';// ::showcaseReactIcons
+// import PageCurriculumFlashcardParser from './pages/PageCurriculumFlashcardParser';// ::curriculumFlashcardParser
+// import PageImportAssessmentJSONIntoSQLite from './pages/PageImportAssessmentJSONIntoSQLite';// ::importAssessmentJSONIntoSQLite
+// import PageShowcaseSampleDataWithFaker from './pages/PageShowcaseSampleDataWithFaker';// ::showcaseSampleDataWithFaker
+// import PageShowcaseMongoDBDriverCRUD from './pages/PageShowcaseMongoDBDriverCRUD';// ::showcaseMongoDBDriverCRUD
+// import PageGenerateMockData from './pages/PageGenerateMockData';// ::generateMockData
+// import PageShowcaseMongooseCRUD from './pages/PageShowcaseMongooseCRUD';// ::showcaseMongooseCRUD
+// import PageHowtos from '../custom/pages/PageHowtos';// ::howtos
+// import PageFlashcards from './pages/PageFlashcards';// ::flashcards
 
 import pages from './data/json/itemTypes/itemType_pages.json';
 
@@ -41,48 +41,30 @@ const userHasAccess = (pageIdCode: string) => {
 function Site() {
 
 	return (
-		<Router>
-			<div className='app_site'>
-				<div className="topHeader">
-					<h1 className='siteTitle'>Edward Tanguay</h1>
-				</div>
+		<div className='app_site'>
+			<div className="topHeader">
+				<h1 className='siteTitle'>Edward Tanguay</h1>
+			</div>
+			<BrowserRouter>
 				<nav>
 					{displayOrderSortedPages.filter((page: any) => page.environment === 'frontendOnly' || config.getSiteMode() === 'development').map((page: any, i: number) => {
 						const smartIdCode = page.idCode === 'home' ? '/' : '/' + page.idCode;
-						return <span key={i}><Link to={`${smartIdCode}`}>{page.title}</Link></span>
+						return <span key={i}><NavLink to={`${smartIdCode}`}>{page.title}</NavLink></span>
 					})}
 				</nav>
 
 				<section className='app_container'>
-					<Switch>
-						<Route exact path='/'><PageHome /></Route>
+					<Routes>
+						<Route path='/' element={<PageHome />} />
 						{/* DYNAMIC_JSX_AREA: routePageComponentLines */}
-						<Route path='/showcaseCounterUsingState'>{userHasAccess('showcaseCounterUsingState') && <PageShowcaseCounterUsingState />}</Route> {/* ::showcaseCounterUsingState */}
-						<Route path='/showcaseCounterUsingReducer'>{userHasAccess('showcaseCounterUsingReducer') && <PageShowcaseCounterUsingReducer />}</Route> {/* ::showcaseCounterUsingReducer */}
-						<Route path='/showcaseTypeScriptClasses'>{userHasAccess('showcaseTypeScriptClasses') && <PageShowcaseTypeScriptClasses />}</Route> {/* ::showcaseTypeScriptClasses */}
-						<Route path='/showcaseJavaScriptComponent'>{userHasAccess('showcaseJavaScriptComponent') && <PageShowcaseJavaScriptComponent />}</Route> {/* ::showcaseJavaScriptComponent */}
-						<Route path='/showcaseSqliteReader'>{userHasAccess('showcaseSqliteReader') && <PageShowcaseSqliteReader />}</Route> {/* ::showcaseSqliteReader */}
-						<Route path='/showcaseJsonReadWrite'>{userHasAccess('showcaseJsonReadWrite') && <PageShowcaseJsonReadWrite />}</Route> {/* ::showcaseJsonReadWrite */}
-						<Route path='/deletePage'>{userHasAccess('deletePage') && <PageDeletePage />}</Route> {/* ::deletePage */}
-						<Route path='/createPage'>{userHasAccess('createPage') && <PageCreatePage />}</Route> {/* ::createPage */}
-						<Route path='/showcaseNewsApi'>{userHasAccess('showcaseNewsApi') && <PageShowcaseNewsApi />}</Route> {/* ::showcaseNewsApi */}
-						<Route path='/manageShowcaseReports'>{userHasAccess('manageShowcaseReports') && <PageManageShowcaseReports />}</Route> {/* ::manageShowcaseReports */}
-						<Route path='/showcaseUseToggle'>{userHasAccess('showcaseUseToggle') && <PageShowcaseUseToggle />}</Route> {/* ::showcaseUseToggle */}
-						<Route path='/showcaseClassInState'>{userHasAccess('showcaseClassInState') && <PageShowcaseClassInState />}</Route> {/* ::showcaseClassInState */}
-						<Route path='/showcaseFetchTryCatch'>{userHasAccess('showcaseFetchTryCatch') && <PageShowcaseFetchTryCatch />}</Route> {/* ::showcaseFetchTryCatch */}
-						<Route path='/showcaseReactIcons'>{userHasAccess('showcaseReactIcons') && <PageShowcaseReactIcons />}</Route> {/* ::showcaseReactIcons */}
-						<Route path='/curriculumFlashcardParser'>{userHasAccess('curriculumFlashcardParser') && <PageCurriculumFlashcardParser />}</Route> {/* ::curriculumFlashcardParser */}
-						<Route path='/importAssessmentJSONIntoSQLite'>{userHasAccess('importAssessmentJSONIntoSQLite') && <PageImportAssessmentJSONIntoSQLite />}</Route> {/* ::importAssessmentJSONIntoSQLite */}
-						<Route path='/showcaseSampleDataWithFaker'>{userHasAccess('showcaseSampleDataWithFaker') && <PageShowcaseSampleDataWithFaker />}</Route> {/* ::showcaseSampleDataWithFaker */}
-						<Route path='/showcaseMongoDBDriverCRUD'>{userHasAccess('showcaseMongoDBDriverCRUD') && <PageShowcaseMongoDBDriverCRUD />}</Route> {/* ::showcaseMongoDBDriverCRUD */}
-						<Route path='/generateMockData'>{userHasAccess('generateMockData') && <PageGenerateMockData />}</Route> {/* ::generateMockData */}
-						<Route path='/showcaseMongooseCRUD'>{userHasAccess('showcaseMongooseCRUD') && <PageShowcaseMongooseCRUD />}</Route> {/* ::showcaseMongooseCRUD */}
-						<Route path='/howtos'>{userHasAccess('howtos') && <PageHowtos />}</Route> {/* ::howtos */}
-						<Route path='/flashcards'>{userHasAccess('flashcards') && <PageFlashcards />}</Route>{/* ::flashcards */}
-					</Switch>
+						{userHasAccess('showcaseCounterUsingState') && <Route path='/showcaseCounterUsingState' element={<PageShowcaseCounterUsingState />} />} {/* ::showcaseCounterUsingState */}
+						{userHasAccess('showcaseCounterUsingReducer') && <Route path='/showcaseCounterUsingReducer' element={<PageShowcaseCounterUsingReducer />} />} {/* ::showcaseCounterUsingReducer */}
+						{userHasAccess('showcaseTypeScriptClasses') && <Route path='/showcaseTypeScriptClasses' element={<PageShowcaseTypeScriptClasses />} />} {/* ::showcaseTypeScriptClasses */}
+
+					</Routes>
 				</section>
-			</div>
-		</Router >
+			</BrowserRouter>
+		</div>
 	);
 }
 
