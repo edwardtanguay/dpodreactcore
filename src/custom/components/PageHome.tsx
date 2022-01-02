ad0151337c38aafd6c602baec5b8e44b54e551a5
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-02 14:38:20 +0100

    home more

--- src/custom/components/PageHome.tsx
+++ src/custom/components/PageHome.tsx
@@ -5,6 +5,7 @@ function PageHome() {
 
 	return (
 		<div className="page page_home">
+			<p>This site is where I publish all the content I create, from code examples, how-to instructions, to excerpts from books I am reading, notes from courses I take, etc.</p>
 			<p>This site will eventually replace <a href="https://onespace.netlify.app">https://onespace.netlify.app</a> </p>
 			<p>If you can't find what you are looking for here, check out the onespace link.</p>
 		</div>
