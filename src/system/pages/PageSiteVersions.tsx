9932fe71da6e5e49ef1d42dce04e31428c08863d
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-10 15:04:59 +0200

    page finished

--- src/system/pages/PageSiteVersions.tsx
+++ src/system/pages/PageSiteVersions.tsx
@@ -10,17 +10,27 @@ function PageSiteVersions() {
 		<div className="page page_siteVersions">
 			<h2 className="title">Datapod-for-React CORE</h2>
 			<h3>Version history</h3>
+			<div className="invite">
+				You want to help me develop this?{' '}
+				<a
+					href="https://github.com/edwardtanguay/dpodreactcore"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Fork, code, create a pull-request
+				</a>
+			</div>
 			<p className="versions">
 				<ul>
 					{siteVersions.map((sv, index) => {
 						return (
 							<li key={index} className="siteVersion">
 								<span className="version">{sv.version}</span>
-								<span className="separator">{' '}-{' '}</span>
+								<span className="separator"> - </span>
 								<span className="whenPublished">
 									{sv.whenPublished}
 								</span>
-								<span className="separator">{' '}-{' '}</span>
+								<span className="separator"> - </span>
 								<span className="shortDescription">
 									{sv.shortDescription}
 								</span>
